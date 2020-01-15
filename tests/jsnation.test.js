20fc0a6e239c3d8bb44639eeb04c74139eaaaee7
Oleg Proskurin <regx@usul.su>
2020-01-15 18:22:12 +0300

    Update tests

--- tests/jsnation.test.js
+++ tests/jsnation.test.js
@@ -1,8 +1,9 @@
 import { getContent } from '../src';
+import settings from './jsnation.conference-settings'
 
 describe('JsNation', () => {
   it('should render content', async () => {
-    const content = await getContent();
+    const content = await getContent(settings);
 
     expect(content).toMatchSnapshot();
   });
