8077a6f5c28b0527f9257dc6e0dce278cebecdfc
Denis Urban <denis.urban@gitnation.org>
2023-02-21 14:05:09 +0300

    downgrade node-fetch, change base url

--- src/http-utils.js
+++ src/http-utils.js
@@ -1,7 +1,6 @@
 const fetch = require('node-fetch');
 
-// const BASE_URL = 'https://ems.gitnation.org';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://ems.gitnation.org';
 
 const getSpeakers = async (eventId) => {
     if (!eventId) {
